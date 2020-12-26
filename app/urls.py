8b1067f8a02e9c5bf6831656c12facbbfddbd203
kusuharateppei <21931044tk@tama.ac.jp>
2020-12-26 18:49:56 +0900

    chenged a app

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ from .views import ItemFilterView, ItemDetailView, ItemCreateView, ItemUpdateVie
 urlpatterns = [
     path("", ItemFilterView.as_view(), name="index"),
     path("detail/<int:pk>/", ItemDetailView.as_view(), name="detail"),
-    path("create/", ItemCreateView.as_view(), name="detail"),
+    path("create/", ItemCreateView.as_view(), name="create"),
     path("update/<int:pk>", ItemUpdateView.as_view(), name="update"),
     path("detete/<int:pk>", ItemDeleteView.as_view(), name="delete"),
 ]
